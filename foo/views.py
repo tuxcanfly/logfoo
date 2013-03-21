af44d5c5a85961f90f54aa4cb17e2aa5073c1a76
Javed Khan <javed@agiliq.com>
2013-03-21 15:23:16 +0530

    crud

--- foo/views.py
+++ foo/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from foo.models import Bar
+
+
+class BarCreateView(CreateView):
+    model = Bar
+
+    def get_success_url(self):
+        return '/'
+
+
+class BarListView(ListView):
+    model = Bar
--- logfoo/urls.py
+++ logfoo/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 
+from foo.views import BarCreateView, BarListView
+
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -7,7 +10,8 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'logfoo.views.home', name='home'),
-    # url(r'^logfoo/', include('logfoo.foo.urls')),
+    url(r'^foo/', BarCreateView.as_view()),
+    url(r'^', BarListView.as_view()),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
